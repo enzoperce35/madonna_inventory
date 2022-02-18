f881006f161b148ea51561414cd2fd38521716dd
enzoperce35 <enzoperce35@gmail.com>
2022-02-19 06:01:55 +0800

    added burger toggle bars

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def user_is_admin
+    !current_user.branch.nil?
+  end
+  
+  def branch_name
+    if !user_signed_in? || !user_is_admin
+      ''
+    else
+      current_user.branch.prepend(' - ')
+    end
+  end
 end
