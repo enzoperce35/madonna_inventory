eb9b3175c7cab93724d05b2ba6fabbd487d4ab43
enzoperce35 <enzoperce35@gmail.com>
2022-02-22 18:51:59 +0800

    minor modifications

--- db/migrate/20220219232347_create_inventory_items.rb
+++ db/migrate/20220219232347_create_inventory_items.rb
@@ -2,10 +2,11 @@ class CreateInventoryItems < ActiveRecord::Migration[6.1]
   def change
     create_table :inventory_items do |t|
       t.string :name
-      t.string :category
+      t.string :item_type
       t.string :unit
       t.float :margin
       t.float :stock
+      t.timestamp :re_stock
 
       t.timestamps
     end
